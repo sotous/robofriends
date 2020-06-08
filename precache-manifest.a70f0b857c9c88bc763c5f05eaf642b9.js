d758a920fcd37b9f70a7cd53f97534c1cc1872f4
Jesus Soto <sotous0@gmail.com>
2020-06-08 17:45:34 -0500

    github pages PWA

---
+++ precache-manifest.a70f0b857c9c88bc763c5f05eaf642b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7e65453e2c9130fbda09873e513544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71129027fd56c3d797ab",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "60a06cbe9f074a4cb8f8",
+    "url": "/static/css/main.2cf75415.chunk.css"
+  },
+  {
+    "revision": "71129027fd56c3d797ab",
+    "url": "/static/js/2.303fca07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.303fca07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a06cbe9f074a4cb8f8",
+    "url": "/static/js/main.6031d763.chunk.js"
+  },
+  {
+    "revision": "700cce3a7caf4f992d6c",
+    "url": "/static/js/runtime-main.ebe91ffd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.bf35aa50bba96db90c07249ee6e7c281.js"
+  "/precache-manifest.a70f0b857c9c88bc763c5f05eaf642b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
