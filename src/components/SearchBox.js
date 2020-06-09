4c54126ff1b90b1e7734a2d46b352f90a821281d
Jesus Soto <sotous0@gmail.com>
2020-06-08 21:12:45 -0500

    added aria-label attribute instead of the label tag

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,16 +3,14 @@ import React from 'react';
 const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
-      <div className='flex items-center flex-column'>
-        <label className="w-25 pa3 washed-blue" for='searchBox'>Search for robots</label>    
-        <input
-          id='searchBox'
-          className='w-third pa3 ba b--green bg-lightest-blue'
-          type='search'
-          placeholder='name'
-          onChange={searchChange}
-        />
-      </div>
+      <input
+        id='searchBox'
+        className='w-third pa3 ba b--green bg-lightest-blue'
+        aria-label='Search robots'
+        type='search'
+        placeholder='Search robots'
+        onChange={searchChange}
+      />
     </div>
   );
 }
