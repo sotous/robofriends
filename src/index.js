42c3311003ff03124173d61035661d2d6566dd9e
Jesus Soto <sotous0@gmail.com>
2020-06-08 20:11:33 -0500

    updated service worker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from "./serviceWorker";
 import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 
@@ -24,4 +24,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
+serviceWorker.unregister();
